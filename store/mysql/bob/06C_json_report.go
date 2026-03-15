4f7aea5b1a30ef2e9460df988fe47847dd6ed8df
Andrew Sellers <andrew@andrewcsellers.com>
2026-03-15 10:27:01 -0500

    in progress json report

--- store/mysql/bob/06C_json_report.go
+++ store/mysql/bob/06C_json_report.go
@@ -1,7 +1,36 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/acsellers/golang-db-compare/store/common"
+	"github.com/acsellers/golang-db-compare/store/mysql/bob/models"
+)
 
 func JSONReport(w http.ResponseWriter, r *http.Request) {
+	locationID := r.URL.Query().Get("location")
+	startDate := r.URL.Query().Get("start_date")
+	endDate := r.URL.Query().Get("end_date")
+	locationIDInt, _ := strconv.Atoi(locationID)
+	startDateObj, _ := time.Parse("2006-01-02", startDate)
+	endDateObj, _ := time.Parse("2006-01-02", endDate)
+	if locationIDInt == 0 || startDateObj.IsZero() || endDateObj.IsZero() {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	orders, err := models.Orders.Query(
+		models.SelectThenLoad.Order.OrderPayments(),
+		models.SelectWhere.Orders.OrderDate.GT(startDateObj),
+		models.SelectWhere.Orders.OrderDate.LT(endDateObj),
+		models.SelectWhere.Orders.LocationID.EQ(int64(locationIDInt)),
+	).All(r.Context(), db)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	ret := make([]common.LocationPaymentInfo, 0, len(orders))
 
 }
